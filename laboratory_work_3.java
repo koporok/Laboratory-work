ad90186daa8a7fe571efb2efba484fdf3c7db514
koporok <122740702+koporok@users.noreply.github.com>
2023-02-22 17:06:50 +0300

    Create laboratory_work_3.java

---
+++ laboratory_work_3.java
@@ -0,0 +1,12 @@
+public class laboratory_work_3 {
+    public static void main(String[] args){
+        for (int i = 1; i < 11; i++){
+            int y;
+            System.out.println("Умножение на "+i);
+            for (int j = 1; j < 11; j++){
+                y = i * j;
+                System.out.println(i+"*"+j+"="+y);
+            }
+        }
+    }
+}
