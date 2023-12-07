2ab6c7f0ef27e76bfff86561c4111ab86b02ddfc
koporok <122740702+koporok@users.noreply.github.com>
2023-12-07 12:39:31 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,42 +1,31 @@
-import java.io.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main {
-
-    public static void main(String[] args) throws IOException {
-        // Прочитайте содержимое файла с помощью BufferedReader
-        BufferedReader reader = new BufferedReader(new FileReader("24_demo.txt"));
-
-        // Создайте шаблон регулярного выражения для поиска последовательных символов X
-        Pattern pattern = Pattern.compile("X{1,}", Pattern.DOTALL);
-
-        int c;
-        StringBuilder text = new StringBuilder();
-
-        // Считайте файл посимвольно и добавляйте в StringBuilder
-        while ((c = reader.read()) != -1) {
-            text.append((char) c);
-        }
-
-        //Используйте шаблон для поиска совпадений в тексте
-        Matcher matcher = pattern.matcher(text);
-
-        int matchLength;
-        int maxMatchLength = 0;
-
-        // Найдите максимальную длину совпадения
-        while (matcher.find()){
-            matchLength = (matcher.group()).length();
-            if(matchLength > maxMatchLength){
-                maxMatchLength = matchLength;
+public class Main{
+    public static void main(String[] args) {
+        try {
+            // Создание нового процесса
+            Process process;
+            if (System.getProperty("os.name").startsWith("Windows")) {
+                process = Runtime.getRuntime().exec("cmd /c dir");
+            } else {
+                process = Runtime.getRuntime().exec("ls");
             }
+
+            // Получение вывода процесса
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            // Ожидание завершения процесса и получение кода завершения
+            int exitCode = process.waitFor();
+            System.out.println("\nКод завершения: " + exitCode);
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
         }
-
-        // Выведите максимальную длину совпадения
-        System.out.println(maxMatchLength);
-
-        // Закройте программу чтения, чтобы освободить ресурсы
-        reader.close();
     }
 }
